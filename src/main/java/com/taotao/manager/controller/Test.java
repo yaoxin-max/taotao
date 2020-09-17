e760538e627de969f76d9809d5c8d3cc19694fbc
yaoxin <yaoxin@qq.com>
2020-09-17 13:18:57 +0800

    Revert "删除"
    This reverts commit f4d4e89b2e1e7a115c7d22a83d4bf52b4be26d90.

---
+++ src/main/java/com/taotao/manager/controller/Test.java
@@ -0,0 +1,10 @@
+package com.taotao.manager.controller;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println(123);
+	System.out.println(456);
+	System.out.println(789);
+	System.out.println("test");
+}
+}
