3c7376420971cc6deaeaec664db4097a0fa3767d
yaoxin-sudo <yx17693121776@163.com>
2020-10-25 11:16:26 +0800

    解决冲突11

--- src/main/java/com/taotao/manager/controller/Test.java
+++ src/main/java/com/taotao/manager/controller/Test.java
@@ -2,10 +2,11 @@ package com.taotao.manager.controller;
 
 public class Test {
 	public static void main(String[] args) {
+
 		System.out.println(111);
 		System.out.println(333);
 		System.out.println(2222);	
-		
+
 	}
 
 }
